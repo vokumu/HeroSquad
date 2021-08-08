56f553d8a88ad79c95708958363c0c8832e6e8d6
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-08 07:28:49 +0300

    2.3 Added the Hero Test class

--- src/main/java/module/Hero.java
+++ src/main/java/module/Hero.java
@@ -1,5 +1,48 @@
 package module;
 
 public class Hero {
+    private int id;
+    private String name;
+    private int age;
+    private String specialPowers;
+    private  String weakness;
+
+    public Hero(String name,int age,String specialPowers,String weakness){
+        this.name=name;
+        this.age=age;
+        this.specialPowers=specialPowers;
+        this.weakness=weakness;
+
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void age(int age){
+        this.age=age;
+    }
+    public void setSpecialPowers(String specialPowers){
+        this.specialPowers=specialPowers;
+    }
+    public  void setWeakness(String weakness){
+        this.weakness=weakness;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getSpecialPowers(){
+        return specialPowers;
+    }
+    public String getWeakness(){
+        return weakness;
+    }
 
 }
--- src/test/java/module/HeroTest.java
+++ src/test/java/module/HeroTest.java
@@ -1,7 +1,43 @@
 package module;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
+    @Test
+    public void NewSuperHeroObjectGetsCorrectlyCreated_true() throws  Exception{
+        Hero hero=setupNewSuperHero();
+        assertEquals(true,hero instanceof Hero );
+    }
+    @Test
+    public void SuperHeroInstantiatesWithName_true() throws  Exception {
+        Hero hero=setupNewSuperHero();
+        assertEquals("Vicky",hero.getName());
+    }
+
+    @Test
+    public void SuperHeroInstantiatesWithAge_true() throws  Exception {
+        Hero hero=setupNewSuperHero();
+        assertEquals(23,hero.getAge());
+
+    }
+
+    @Test
+    public void SuperHeroInstantiatesWithSpecialPowers_true() throws Exception {
+        Hero hero=setupNewSuperHero();
+        assertEquals("speed",hero.getSpecialPowers());
+    }
+
+    @Test
+    public void SuperHeroInstantiatesWithWeakness_true() throws Exception{
+        Hero hero=setupNewSuperHero();
+        assertEquals("rice",hero.getWeakness());
+    }
+
+    //helper methods
+    public Hero setupNewSuperHero(){
+        return new Hero("Vicky",23,"speed","rice");
+    }
 
 }
