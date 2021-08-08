1eccb59f9aac329c73abcd5a865875251a008c69
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-08 07:56:06 +0300

    3.2 Added the  Squad constructor

--- src/main/java/module/Squad.java
+++ src/main/java/module/Squad.java
@@ -4,4 +4,11 @@ public class Squad {
     private int maxSize;
     private String name;
     private String fightingCause;
+
+    public Squad(int maxSize,String name,String fightingCause){
+        this.maxSize=maxSize;
+        this.name=name;
+        this.fightingCause=fightingCause;
+
+    }
 }
