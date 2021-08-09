967a13b7809b310da630c210eda29e3a47712951
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-09 04:26:45 +0300

    Added the navigation to Add Sqaud

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,10 @@ public class App {
             return new ModelAndView(model, "home.hbs"); // assemble individual pieces and render
         }, new HandlebarsTemplateEngine()); //
 
+        get("/addSquad", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "addSqaud.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
