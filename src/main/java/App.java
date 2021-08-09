11af7a637dad6999b08f0823c4822d6f3909b168
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-09 04:04:34 +0300

    Added the template layout

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,15 @@
-import static spark.Spark.*;
+import java.util.HashMap;
 
+import spark.ModelAndView;
+
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         get("/hello", (request, response) -> "Hello Friend!");
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
