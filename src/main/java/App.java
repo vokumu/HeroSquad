6f75452a05ac12d10b37654285bb74077756f3b5
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-09 04:11:27 +0300

    Added the navigation to home

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,16 @@
 import java.util.HashMap;
 
 import spark.ModelAndView;
-
+import java.util.Map;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/hello", (request, response) -> "Hello Friend!");
-        get("/", (request, response) -> {
-            return new ModelAndView(new HashMap(), "index.hbs");
-        }, new HandlebarsTemplateEngine());
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "home.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+
     }
 }
